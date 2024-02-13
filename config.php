fe1eb16323d79e9bca68611ed5c79552e23c1f6b
A. ADELL <aabd2006@gmail.com>
2024-02-13 13:12:30 +0100

    push all version 0 web pages

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+//Create session per user:
+session_start();
+
+
+
+//define some constants:
+define('ROOT_PATH', realpath(dirname(__FILE__)));
+define('BASE_URL', 'http://localhost:2024/');
+
+// connect to the databse:
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php include('config.php'); ?>
+<?php include('includes/public/head_section.php'); ?>
+<title>MyWebSite | Home </title>
+
+</head>
+
+<body>
+
+	<div class="container">
+
+		<!-- Navbar -->
+		<?php include(ROOT_PATH . '/includes/public/navbar.php'); ?>
+		<!-- // Navbar -->
+
+		<!-- Banner -->
+		<?php include(ROOT_PATH . '/includes/public/banner.php'); ?>
+		<!-- // Banner -->
+
+		<!-- Messages -->
+		
+		<!-- // Messages -->
+
+		<!-- content -->
+		<div class="content">
+			<h2 class="content-title">Recent Articles</h2>
+			<hr>
+
+			
+
+
+
+		</div>
+		<!-- // content -->
+
+
+	</div>
+	<!-- // container -->
+
+
+	<!-- Footer -->
+	<?php include(ROOT_PATH . '/includes/public/footer.php'); ?>
+	<!-- // Footer -->
